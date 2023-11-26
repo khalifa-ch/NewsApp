3b587609be068d697dced5863869082c8fd9680e
khalifa-ch <khalifachelbi06@gmail.com>
2023-11-26 16:32:00 +0100

    first commit

---
+++ app/src/main/java/com/example/newsappandroid/HomeFragment.java
@@ -0,0 +1,23 @@
+package com.example.newsappandroid;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+    String api ="76427b0adf0146629cfc88e1b34598d5";
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.homefragment,null);
+        return  v;
+    }
+}
